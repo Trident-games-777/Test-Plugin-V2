eb95189f560c91f1f6cba777f96a71733dff6e04
Moiseiev Yevhen <yevhen.moiseiev@itrident.agency>
2025-07-31 12:05:19 +0300

    еее

--- app/src/main/java/com/example/testpluginv2/MainActivity.kt
+++ app/src/main/java/com/example/testpluginv2/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.testpluginv2.composables.MyAppNavHost
+import kotlinx.coroutines.launch
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        lifecycleScope.launch {  }
         setContent {
             MyAppNavHost()
         }
